39f543e5e2caf223162eec479d2ce0f824b5bf1b
akashvarshney <akash.varshney@arcadis.com>
2022-01-08 19:55:55 +0530

    Add project files.

---
+++ LinkedList/DoublyLinkedList.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structure.DoublyLinkedList
+{
+    public class DoublyLinkedList
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+
+        public int Length { get; set; }
+
+        public void AppendLast(int val)
+        {
+            if (Head == null)
+            {
+                Head = new Node(val);
+                Tail = Head;
+            }
+            else
+            {
+                Node node = new Node(val);
+                Tail.Next = node;
+                Tail.Previous = Tail;
+                Tail = node;
+            }
+
+            Length++;
+        }
+
+        public void AppendFirst(int val)
+        {
+            if (Head == null)
+            {
+                Head = new Node(val);
+                Tail = Head;
+            }
+            else
+            {
+                Node first = new Node(val);
+                Head.Previous = first;
+                first.Next = Head;
+                Head = first;
+            }
+            Length++;
+        }
+
+        public void InsertAt(int index, int val)
+        {
+            if (index == 0)
+            {
+                AppendFirst(val);
+            }
+            else if (index == Length - 1)
+            {
+                AppendLast(val);
+            }
+            else
+            {
+                Node vtx = new Node(val);
+                Node pre = Head;
+
+                Node aft = pre.Next;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    pre = pre.Next;
+                }
+                vtx.Next = aft;
+                aft.Previous = vtx;
+                vtx.Previous = pre;
+                pre.Next = vtx;
+
+                Length++;
+            }
+        }
+
+    }
+
+    public class Node
+    {
+        public Node Previous { get; set; }
+        public Node Next { get; set; }
+        public int Value { get; set; }
+
+
+        public Node(int val)
+        {
+            this.Value = val;
+            this.Next = null;
+            this.Previous = null;
+        }
+    }    
+}
---
+++ LinkedList/SinglyLinkedList.cs
@@ -0,0 +1,173 @@
+﻿namespace data_structure.SinglyLinkedList
+{
+    public class SinglyLinkedList
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// Adding value to last of linked list 
+        /// Making sure compexity is O(1)
+        /// </summary>
+        /// <param name="val"></param>
+        public void AddLast(int val)
+        {
+            if (Head == null)
+            {
+                Head = new Node(val);
+                Tail = this.Head;
+            }
+            else
+            {
+                // 10-->12-->23-->65-->
+                Node newNode = new Node(val);
+                Tail.Next = newNode;
+                Tail = newNode;
+            }
+            Count++;
+        }
+
+        /// <summary>
+        /// Adding @ first Place 
+        /// Time Complexity O(1)
+        /// </summary>
+        /// <param name="val"></param>
+        public void AddFirst(int val)
+        {
+            if (Head == null)
+            {
+                Head = new Node(val);
+                Tail = this.Head;
+            }
+            else
+            {
+                Node node = new Node(val);
+                node.Next = Head;
+                Head = node;
+            }
+            Count++;
+        }
+
+        /// <summary>
+        /// Insert at index 
+        /// Time complexity id O(n)
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="val"></param>
+        public void InsertAt(int index, int val)
+        {
+            if (index >= Count)
+            {
+                Console.WriteLine("Index out of range");
+            }
+            else
+            {
+                if (index == 0)
+                {
+                    AddFirst(val);
+                }
+                else if (index == Count - 1)
+                {
+                    AddLast(val);
+                }
+                else
+                {
+                    Node Pre = Head;
+
+                    //Need to travese the linked List till index -1
+                    for (int i = 0; i < index - 1; i++)
+                    {
+                        Pre = Pre.Next;
+                    }
+                    Node After = Pre.Next;
+                    Node newNode = new Node(val);
+                    Pre.Next = newNode;
+                    newNode.Next = After;
+
+                    Count++;
+                }
+            }
+        }
+
+        public void RemoveFirst()
+        {
+            if (Head == null)
+            {
+                Console.WriteLine("Emptry Linked List");
+            }
+            else
+            {
+                Node temp = Head.Next;
+                Head = temp;
+            }
+            Count--;
+        }
+
+        public void RemoveLast()
+        {
+            if (Head == null)
+            {
+                Console.WriteLine("Empty Linked List");
+            }
+            else
+            {
+                Node pre = Head;
+                Node temp = Head.Next;
+                while (temp.Next != null)
+                {
+                    pre = pre.Next;
+                }
+                pre.Next = null;
+                Tail = pre;
+                Count--;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= Count)
+            {
+                Console.WriteLine("Index out of range");
+            }
+            else
+            {
+                if (index == 0)
+                {
+                    RemoveFirst();
+                }
+                else if (index == Count - 1)
+                {
+                    RemoveLast();
+                }
+                else
+                {
+                    Node Pre = Head;
+                    Node After = Pre.Next;
+                    //Need to travese the linked List till index -1
+                    for (int i = 0; i < index - 1; i++)
+                    {
+                        Pre = Pre.Next;
+                        After = After.Next;
+                    }
+
+                    Pre.Next = After;
+                    Count--;
+                }
+            }
+        }
+    }
+
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int val)
+        {
+            Value = val;
+            Next = null;
+        }
+    }
+
+}
