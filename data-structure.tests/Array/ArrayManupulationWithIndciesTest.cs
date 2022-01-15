55d63b57a486997d240b1999fe7a3fd3a62836ca
Akash Varshney <akash.varshney@gmail.com>
2022-01-15 18:06:30 +0530

    added Array manpulation

---
+++ data-structure.tests/Array/ArrayManupulationWithIndciesTest.cs
@@ -0,0 +1,30 @@
+﻿using data_structure.Arrays;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structure.tests.Array
+{
+    [TestClass]
+    public class ArrayManupulationWithIndciesTest
+    {
+        [TestMethod]
+        public void ArrayManupulationTest()
+        {
+            //arrange 
+            int arrayLength = 5;
+            List<List<int>> list = new List<List<int>>() {
+                new List<int> { 1, 2, 100 },
+                new List<int> { 2, 5, 100 },
+                new List<int> { 3, 4, 100 }
+            };
+
+            int result = ArrayManupulationWithIndcies.ArrayManupulation(arrayLength, list);
+
+
+        }
+    }
+}
---
+++ data-structure/Arrays/ArrayManupulationWithIndcies.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structure.Arrays
+{
+    public class ArrayManupulationWithIndcies
+    {
+        public static int ArrayManupulation(int n, List<List<int>> queries)
+        {
+            int a = 0;
+            int b = 1;
+            int k = 2;
+            
+            int[] array = new int[n];
+            foreach (var item in queries)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (item[a] - 1 <= i && item[b] - 1 >= i)
+                        array[i] = array[i] + item[k];
+                }   
+            }
+            return array.Max();
+        }
+    }
+}
